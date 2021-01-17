c9ed7ff0aa86bb236ed49c1d88103a51db1dc415
Sukrat Kashyap <kashyapsukrat@gmail.com>
2021-01-17 23:01:33 +0000

    feat: done day 3

---
+++ solutions/adventofcode/2020/day03-1/main.ts
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch'
+
+fetch("https://adventofcode.com/2020/day/3/input",
+    {
+        headers: {
+            "cookie": `session=${process.env.ADVENTOFCODE};`
+        }
+    })
+    .then(res => {
+        return res.text()
+    }).then(data => {
+        const answer = solve(data)
+        console.log(answer)
+    })
+
+const regex = /(\d+)-(\d+) (\w+): (\w+)/
+
+function solve(str: string): number {
+    let rows = str.split('\n')
+    const map = rows.map(m => m.split(''))
+
+    let pos = 0;
+    let count = 0;
+    for (const [index, row] of map.entries()) {
+        // skipping empty line
+        if (row.length === 0) {
+            continue
+        }
+        let whatsThere = row[pos]
+        if (whatsThere === "#") {
+            count += 1
+        }
+        pos = (pos + 3) % row.length
+    }
+    return count
+}
---
+++ solutions/adventofcode/2020/day03-2/main.ts
@@ -0,0 +1,45 @@
+import fetch from 'node-fetch'
+
+fetch("https://adventofcode.com/2020/day/3/input",
+    {
+        headers: {
+            "cookie": `session=${process.env.ADVENTOFCODE};`
+        }
+    })
+    .then(res => {
+        return res.text()
+    }).then(data => {
+        const answer = solve(data)
+        console.log(answer)
+    })
+
+const regex = /(\d+)-(\d+) (\w+): (\w+)/
+
+function solve(str: string): number {
+    let rows = str.split('\n')
+    const map = rows.map(m => m.split(''))
+
+    return solveFor(map, 1, 1)
+        * solveFor(map, 3, 1)
+        * solveFor(map, 5, 1)
+        * solveFor(map, 7, 1)
+        * solveFor(map, 1, 2)
+}
+
+function solveFor(map: string[][], right: number, down: number): number {
+    let pos = 0;
+    let count = 0;
+    for (let index = 0; index < map.length; index += down) {
+        const row = map[index]
+        // skipping empty line
+        if (row.length === 0) {
+            continue
+        }
+        let whatsThere = row[pos]
+        if (whatsThere === "#") {
+            count += 1
+        }
+        pos = (pos + right) % row.length
+    }
+    return count
+}
