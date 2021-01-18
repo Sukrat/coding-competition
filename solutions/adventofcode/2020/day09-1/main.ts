2aae9045ca4ee7cc5328d08e64fcd329fcefd1c5
Sukrat Kashyap <kashyapsukrat@gmail.com>
2021-01-18 20:36:40 +0000

    day 9

---
+++ solutions/adventofcode/2020/day09-1/main.ts
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch'
+
+fetch("https://adventofcode.com/2020/day/9/input",
+    {
+        headers: {
+            "cookie": `session=${process.env.ADVENTOFCODE};`
+        }
+    })
+    .then(res => {
+        return res.text()
+    }).then(data => {
+        const answer = solve(data)
+        console.log(answer)
+    })
+
+function solve(str: string): number | undefined {
+    const numbers = str.trim().split('\n')
+        .map(m => parseInt(m))
+    const preamble = 25
+
+    const prevNumbers = new Set<number>(numbers.slice(0, preamble))
+    for (let index = preamble; index < numbers.length; index++) {
+        const target = numbers[index]
+        if (!findPair(target, prevNumbers)) {
+            return target
+        }
+        prevNumbers.delete(numbers[index - preamble])
+        prevNumbers.add(target)
+    }
+    return undefined
+}
+
+function findPair(target: number, prev: Set<number>): boolean {
+    for (const num of prev) {
+        const need = target - num
+        if (prev.has(need)) {
+            return true
+        }
+    }
+    return false
+}
